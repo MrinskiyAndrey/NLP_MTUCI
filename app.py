624517dfe558d4887c82587babd8190ac13bf4c1
MrinskiyAndrey <Nomarg@yandex.ru>
2023-11-25 15:16:02 +0300

    Version from 25.11

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ ML_result = "Негативный"
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    # post = Post.query
     if request.method == 'POST':
         comment = request.form['comment']
         try:
@@ -29,15 +28,26 @@ def index():
         except:
             like = None
 
+        #postgresql://postgres:postgres@flask_db:5432/postgres
+
         if request.form['btn'] == 'to_ml':
-            try:
-                print(comment)
-                #ML_result = request.get_json(pass)
-                return render_template('index.html', retry_comment=comment, ML_result=ML_result)
+            # try:
+            #     print(comment)
+            #     #ML_result = request.get_json(pass)
+            #     return render_template('index.html', retry_comment=comment, ML_result=ML_result)
+            #
+            # except:
+            #     return "Ошибка при обращении к ML!"
+            conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="flask_db")
+            cursor = conn.cursor()
 
-            except:
-                return "Ошибка при обращении к ML!"
+            cursor.execute("SELECT * FROM public.post")
+            rows = cursor.fetchall()
 
+            cursor.close()  # закрываем курсор
+            conn.close()  # закрываем подключение
+
+            return render_template('index.html', retry_comment=comment, ml_result=rows)
         elif request.form['btn'] == 'to_bd':
             post = Post(comment=comment, like=like)
             try:
@@ -49,6 +59,25 @@ def index():
 
         elif request.form['btn'] == 'reset':
             return render_template("index.html")
+
+        elif request.form['btn'] == 'to_learn':
+            # try:
+            #     print(comment)
+            #     #ML_result = request.get_json(pass)
+            #     return render_template('index.html', retry_comment=comment, ML_result=ML_result)
+            #
+            # except:
+            #     return "Ошибка при обращении к ML!"
+            conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="flask_db")
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM public.post")
+            rows = cursor.fetchall()
+
+            cursor.close()  # закрываем курсор
+            conn.close()  # закрываем подключение
+
+            return render_template('index.html', retry_comment=comment, ml_result=rows)
 
     else:
         return render_template("index.html")
